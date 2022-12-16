8f7b62c731a6016cce0ed323ea6708c6cfd53692
sisaacs <steve.isaacs@embeddeddomain.com>
2022-12-16 10:17:56 -0800

    Added macros for manifests, error messages, and requiring variables.

--- macros.mk
+++ macros.mk
@@ -24,6 +24,34 @@ this_segment_dir = \
   $(basename $(dir $(word $(words ${MAKEFILE_LIST}),${MAKEFILE_LIST})))
 
 #+
+# Display messages when VERBOSE is defined.
+# Parameters:
+#  1 = The message to display.
+#-
+ifdef VERBOSE
+define verbose
+    $(info verbose:$(1))
+endef
+endif
+
+#+
+# Add an item to a manifest variable.
+# Parameters:
+#   1 = The list to add to.
+#   2 = The optional variable to declare for the value. Use "null" to skip
+#       declaring a new variable.
+#   3 = The value to add to the list.
+#-
+define add-to-manifest
+  $$(call verbose,Adding $(3) to $(1))
+  ifneq ($(2),null)
+    $$(call verboase,Declaring: $(2))
+    $(2) = $(3)
+  endif
+  $(1) += $(3)
+endef
+
+#+
 # Use this macro to insert new lines into multiline messages.
 #-
 define newline
@@ -46,20 +74,46 @@ else
   endef
 endif
 
+
 #+
-# Use this macro to verify variables are set.
-#  Parameters:
-#    1 = A list of required variables.
+# This private macro is used to verify a single variable exists.
+# If the variable is empty then an error message is appended to ErrorMessages.
+# Parmeters:
+#   1 = The name of the variable.
+#   2 = The module in which it is required.
 #-
 define _require_this
+  $(call verbose,Requiring: $(1))
   $(if ${$(1)},\
     ,\
     $(eval ErrorMessages += Variable $(1) must be defined in: $(2)$(newline))\
   )
 endef
 
+#+
+# Use this macro to verify variables are set.
+#  Parameters:
+#    1 = The make file segment.
+#    2 = A list of required variables.
+#-
 define require
+  $(call verbose,Required in: $(1))
   $(foreach v,$(2),$(call _require_this,$(v), $(1)))
+endef
+
+#+
+# This macro displays a list of accumulated messages if defined.
+# Parameters:
+# NOTE: This is intended to be used as part of a recipe.
+#  1 = The heading for displaying the list of messages.
+#  2 = The list of messages separated by ${newline}.
+#-
+define show-messages
+@if [ -n '${$(2)}' ]; then \
+    echo $(1);\
+    m="${$(2)}";printf "$${m//${newline}/\\n}";\
+    read -p "Press ENTER to continue...";\
+  fi
 endef
 
 #+
@@ -95,3 +149,9 @@ endef
 define basenames_in
   $(foreach file,$(wildcard $(1)),$(basename $(notdir ${file})))
 endef
+
+#+
+# Display the value of any variable.
+#-
+show-%:
+> @echo '$*=$($*)'
