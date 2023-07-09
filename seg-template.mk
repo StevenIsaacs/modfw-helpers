e635ca7d8d119b2244deede51971235428923cc7
sisaacs <steve.isaacs@embeddeddomain.com>
2023-07-09 05:50:52 -0700

    Added help messages to the template.

--- seg-template.mk
+++ seg-template.mk
@@ -2,6 +2,8 @@
 # <purpose for this segment>
 #----------------------------------------------------------------------------
 # The prefix <u> must be unique for all files.
+# +++++
+# Preamble
 ifndef <u>_id
 <u>_id := $(call this-segment-id)
 <u>_seg := $(call this-segment)
@@ -11,12 +13,24 @@ ifndef <u>_id
 $(eval $(call set-segment-context,${<u>_id}))
 
 $(call verbose,Make segment: $(call segment,${<u>_id}))
-
+# -----
 <segment body here>
 
+# +++++
+# Postamble
+ifneq ($(call is-goal,help-${<u>_seg}),)
+$(info Help message variable: help_${<u>_name}_msg)
+define help_${<u>_name}_msg
+<make segment help messages>
+endef
+export help_${<u>_name}_msg
+help-${<u>_seg}:
+> echo "$$help_${<u>_name}_msg" | less
+endif
 # Restore the previous context.
 $(eval $(call set-segment-context,${<u>_prv_id}))
 
 else
-$(call add-message,${<u>_seg} has already been included)
+  $(call add-message,${<u>_seg} has already been included)
 endif
+# -----
